e7d8144de1221306e7c6135ea4d1eb67b955f28f
Taha <taha.farhaj@gmail.com>
2025-06-30 00:18:38 +0500

    update

--- Appointment/Form1.Designer.cs
+++ Appointment/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Appointment
+﻿using System.Windows.Forms;
+
+namespace Appointment
 {
     partial class Form1
     {
@@ -28,47 +30,193 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnUpload = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCheckEnc = new System.Windows.Forms.Button();
+            this.btnSaveForm = new System.Windows.Forms.Button();
+            this.btnDownloadExcel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRateLimit = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.lblCurrentRow = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 99.99999F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 2;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 103);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(800, 450);
+            this.dataGridView1.Size = new System.Drawing.Size(794, 344);
             this.dataGridView1.TabIndex = 0;
             // 
-            // btnUpload
+            // flowLayoutPanel1
             // 
-            this.btnUpload.Location = new System.Drawing.Point(336, 33);
-            this.btnUpload.Name = "btnUpload";
-            this.btnUpload.Size = new System.Drawing.Size(91, 23);
-            this.btnUpload.TabIndex = 1;
-            this.btnUpload.Text = "Upload Excel";
-            this.btnUpload.UseVisualStyleBackColor = true;
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.btnCheckEnc);
+            this.flowLayoutPanel1.Controls.Add(this.btnSaveForm);
+            this.flowLayoutPanel1.Controls.Add(this.btnDownloadExcel);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.txtRateLimit);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.txtInterval);
+            this.flowLayoutPanel1.Controls.Add(this.lblCurrentRow);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(794, 94);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload Excel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnCheckEnc
+            // 
+            this.btnCheckEnc.Location = new System.Drawing.Point(99, 3);
+            this.btnCheckEnc.Name = "btnCheckEnc";
+            this.btnCheckEnc.Size = new System.Drawing.Size(91, 23);
+            this.btnCheckEnc.TabIndex = 1;
+            this.btnCheckEnc.Text = "Check Enc";
+            this.btnCheckEnc.UseVisualStyleBackColor = true;
+            this.btnCheckEnc.Click += new System.EventHandler(this.btnCheckEnc_Click);
+            // 
+            // btnSaveForm
+            // 
+            this.btnSaveForm.Location = new System.Drawing.Point(196, 3);
+            this.btnSaveForm.Name = "btnSaveForm";
+            this.btnSaveForm.Size = new System.Drawing.Size(98, 23);
+            this.btnSaveForm.TabIndex = 8;
+            this.btnSaveForm.Text = "Save Booking";
+            this.btnSaveForm.UseVisualStyleBackColor = true;
+            this.btnSaveForm.Click += new System.EventHandler(this.btnSaveForm_Click);
+            // 
+            // btnDownloadExcel
+            // 
+            this.btnDownloadExcel.Location = new System.Drawing.Point(300, 3);
+            this.btnDownloadExcel.Name = "btnDownloadExcel";
+            this.btnDownloadExcel.Size = new System.Drawing.Size(102, 23);
+            this.btnDownloadExcel.TabIndex = 2;
+            this.btnDownloadExcel.Text = "Download Excel";
+            this.btnDownloadExcel.UseVisualStyleBackColor = true;
+            this.btnDownloadExcel.Click += new System.EventHandler(this.btnDownloadExcel_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(408, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Counter";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtRateLimit
+            // 
+            this.txtRateLimit.Location = new System.Drawing.Point(458, 3);
+            this.txtRateLimit.Name = "txtRateLimit";
+            this.txtRateLimit.Size = new System.Drawing.Size(57, 20);
+            this.txtRateLimit.TabIndex = 3;
+            this.txtRateLimit.Text = "10";
+            this.txtRateLimit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(521, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Interval";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Location = new System.Drawing.Point(569, 3);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(56, 20);
+            this.txtInterval.TabIndex = 4;
+            this.txtInterval.Text = "2";
+            this.txtInterval.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblCurrentRow
+            // 
+            this.lblCurrentRow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCurrentRow.AutoSize = true;
+            this.lblCurrentRow.Location = new System.Drawing.Point(631, 0);
+            this.lblCurrentRow.Name = "lblCurrentRow";
+            this.lblCurrentRow.Size = new System.Drawing.Size(79, 29);
+            this.lblCurrentRow.TabIndex = 7;
+            this.lblCurrentRow.Text = "Current Row #:";
+            this.lblCurrentRow.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnUpload);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCheckEnc;
+        private System.Windows.Forms.Button btnDownloadExcel;
+        private TextBox txtRateLimit;
+        private TextBox txtInterval;
+        private Label label1;
+        private Label label2;
+        private Label lblCurrentRow;
+        private Button btnSaveForm;
     }
 }
 
--- Appointment/Form1.cs
+++ Appointment/Form1.cs
@@ -6,18 +6,22 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
 
 namespace Appointment
 {
     public partial class Form1 : Form
     {
+        private DataTable excelData;
+        private readonly HttpClient httpClient = new HttpClient();
         public Form1()
         {
             InitializeComponent();
-            //ExcelPackage.LicenseContext = System.ComponentModel.LicenseContext.NonCommercial;
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
         }
         private void btnUpload_Click(object sender, EventArgs e)
         {
@@ -26,8 +30,8 @@ namespace Appointment
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 var filePath = openFileDialog.FileName;
-                var dataTable = ReadExcelToDataTable(filePath);
-                dataGridView1.DataSource = dataTable;
+                excelData = ReadExcelToDataTable(filePath);
+                dataGridView1.DataSource = excelData;
             }
         }
 
@@ -58,6 +62,303 @@ namespace Appointment
 
                 return dt;
             }
+        }
+
+        private void btnCheckEnc_Click(object sender, EventArgs e)
+        {
+            //FetchEncValue();
+            if (excelData == null)
+            {
+                MessageBox.Show("Please upload an Excel file first.");
+                return;
+            }
+            for (int i = 0; i < excelData.Rows.Count; i++)
+            {
+                var url = excelData.Rows[i][13].ToString(); // Column N = index 13
+                var enc = excelData.Rows[i][14].ToString(); // Column O = index 14
+
+                lblCurrentRow.Text = $"Processing Row {i + 1} ..."; // +2 to account for header row
+
+                if (string.IsNullOrWhiteSpace(url) || !string.IsNullOrWhiteSpace(enc))
+                    continue;
+
+                //listBoxLog.Items.Add($"Processing Row {i + 2}...");
+                var resultEnc = FetchEncValue(url).Result;
+                excelData.Rows[i][14] = resultEnc;
+                dataGridView1.Refresh(); // Show updates in UI
+
+                //// 2. Call SaveBooking API if enc is valid
+                //var row = excelData.Rows[i];
+                //if (!string.IsNullOrWhiteSpace(resultEnc) && !resultEnc.StartsWith("ERROR"))
+                //{
+                //    string saveResult = CallSaveBookingApiAsync(row).Result;
+                //    row["Save dsta"] = "save";
+                //    dataGridView1.Refresh();
+                //}
+                //else
+                //{
+                //    row["Save dsta"] = "";
+                //}
+            }
+            
+            //string newColumnName = "Enc Value";
+            //if (excelData.Columns.Contains(newColumnName))
+            //    newColumnName += "_" + Guid.NewGuid().ToString("N").Substring(0, 4);
+
+            //excelData.Columns.Add(newColumnName, typeof(string));
+
+            //foreach (DataRow row in excelData.Rows)
+            //{
+            //    row[newColumnName] = "Default";
+            //}
+
+            //dataGridView1.DataSource = null; // Reset to apply
+            //dataGridView1.DataSource = excelData;
+        }
+        private async Task<string> CallSaveBookingApiAsync(DataRow row)
+        {
+            try
+            {
+                string url = row["Url"]?.ToString();
+                var parsed = ParseUrlParameters(url);
+
+                // Extract time and format to HH:mm
+                string rawTime = parsed["time"]; // e.g. 11m00
+                string formattedTime = rawTime.Replace("m", ":"); // 11:00
+
+                // Construct full date
+                string bookDate = $"{parsed["year"]}-{parsed["month"].PadLeft(2, '0')}-{parsed["day"].PadLeft(2, '0')}"; // e.g. 2025-08-21
+
+                var payload = new
+                {
+                    bookdate = bookDate,
+                    booktime = formattedTime,
+                    adults = int.Parse(parsed["adults"]),
+                    children = int.Parse(parsed["children"]),
+                    customers = 1,
+                    couponcode = "",
+                    cid = 12,
+                    bid = int.Parse(parsed["bid"]),
+                    pid = 0,
+                    paymod = "",
+                    ofirstname = row["First-name"]?.ToString(),
+                    olastname = row["Last-name"]?.ToString(),
+                    oemail = "test@example.com", // or get from Excel
+                    ocountry = "PK",
+                    ocity = row["City"]?.ToString(),
+                    oaddress = "",
+                    opostalcode = "",
+                    ophone = row["Mobile*"]?.ToString(),
+                    omobile = "",
+                    ccustom1 = row["Number-of-the-Greek-Decision-(Apofasi)*"]?.ToString(),
+                    ccustom2 = row["Employer's-Name-in-Greece*"]?.ToString(),
+                    ccustom3 = row["Father's-name*"]?.ToString(),
+                    ccustom4 = row["Passport-Number*"]?.ToString(),
+                    ccustom5 = row["Date-of-Expiry-of-Passport*"]?.ToString(),
+                    p1firstname = row["First-name"]?.ToString(),
+                    p1lastname = row["Last-name"]?.ToString(),
+                    ocomments = "",
+                    invoice = 0,
+                    enc = row["Enc"]?.ToString(),
+                    rnd = new Random().Next(1, 100)
+                };
+
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(payload);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                using (var client = new HttpClient())
+                {
+                    HttpResponseMessage response = null;
+                    int retries = 0;
+
+                    do
+                    {
+                        response = await client.PostAsync("https://appointment.mfa.gr/inner.php/en/reservations/aero/makebook", content);
+                        retries++;
+                        btnSaveForm.Text = $"Save Booking {retries}";
+                        if (!response.IsSuccessStatusCode)
+                            await Task.Delay(3000);
+                    } while (!response.IsSuccessStatusCode && retries < 5);
+
+                    if (response.IsSuccessStatusCode)
+                        return "Success";
+                    else
+                        return $"Failed: {response.StatusCode}";
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+        }
+
+        private void btnDownloadExcel_Click(object sender, EventArgs e)
+        {
+            if (excelData == null || excelData.Rows.Count == 0)
+            {
+                MessageBox.Show("No data available to export.");
+                return;
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                Filter = "Excel Files|*.xlsx",
+                FileName = "UpdatedReport.xlsx"
+            };
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (var package = new ExcelPackage())
+                {
+                    var ws = package.Workbook.Worksheets.Add("Sheet1");
+
+                    // Load data from DataTable including headers
+                    ws.Cells["A1"].LoadFromDataTable(excelData, true);
+                    ws.Cells[ws.Dimension.Address].AutoFitColumns();
+
+                    // Save to selected file
+                    package.SaveAs(new FileInfo(sfd.FileName));
+                }
+
+                MessageBox.Show("Excel file downloaded successfully!");
+            }
+        }
+
+        private async Task<string> FetchEncValue(string url)
+        {
+            //string url = "https://appointment.mfa.gr/en/reservations/aero/book/?bid=32&day=21&month=8&year=2025&time=11m00&adults=1&children=0";
+
+            int retryCount = 0;
+            int maxRetries = Convert.ToInt32(txtRateLimit.Text);
+            double timeoutSeconds = Convert.ToDouble(txtInterval.Text);
+
+            var httpClient1 = new HttpClient()
+            {
+                Timeout = TimeSpan.FromSeconds(timeoutSeconds)
+            };
+
+            while (retryCount < maxRetries)
+            {
+                try
+                {
+                    btnCheckEnc.Text = $"Check Enc {retryCount + 1}";
+                    var response = httpClient1.GetAsync(url).Result;
+
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.ServiceUnavailable)
+                    {
+                        retryCount++;
+                        await Task.Delay(1000); // wait before retrying
+                        continue;
+                    }
+
+                    response.EnsureSuccessStatusCode();
+                    string html = await response.Content.ReadAsStringAsync();
+
+                    // Parse HTML to extract enc value using HtmlAgilityPack
+                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                    doc.LoadHtml(html);
+
+                    var encNode = doc.DocumentNode.SelectSingleNode("//input[@name='enc']");
+
+                    if (encNode != null)
+                    {
+                        string encValue = encNode.GetAttributeValue("value", "");
+                        MessageBox.Show("enc: " + encValue);
+                        return encValue;
+                    }
+                    else
+                    {
+                        MessageBox.Show("enc input field not found.");
+                        return "";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    retryCount++;
+                    if (retryCount >= maxRetries)
+                    {
+                        //MessageBox.Show("Failed after "+maxRetries+" attempts. Error: " + ex.Message);
+                        btnCheckEnc.Text = "Check Enc";
+                        return "";
+                    }
+                    Task.Delay(1000).Wait(); // delay before retry
+                }
+            }
+            return "";
+        }
+        private Dictionary<string, string> ParseUrlParameters(string url)
+        {
+            var uri = new Uri(url);
+            var query = ParseQueryString(uri.Query);
+
+            return new Dictionary<string, string>
+            {
+                ["bid"] = query["bid"] ?? "",
+                ["day"] = query["day"] ?? "",
+                ["month"] = query["month"] ?? "",
+                ["year"] = query["year"] ?? "",
+                ["time"] = query["time"] ?? "",
+                ["adults"] = query["adults"] ?? "1",
+                ["children"] = query["children"] ?? "0"
+            };
+        }
+
+        private Dictionary<string, string> ParseQueryString(string url)
+        {
+            var uri = new Uri(url);
+            var query = uri.Query.TrimStart('?').Split('&', (char)StringSplitOptions.RemoveEmptyEntries);
+
+            return query
+                .Select(q => q.Split('='))
+                .Where(q => q.Length == 2)
+                .ToDictionary(q => q[0], q => Uri.UnescapeDataString(q[1]));
+        }
+
+        private void btnSaveForm_Click(object sender, EventArgs e)
+        {
+            if (excelData == null)
+            {
+                MessageBox.Show("Please upload an Excel file first.");
+                return;
+            }
+            for (int i = 0; i < excelData.Rows.Count; i++)
+            {
+                var url = excelData.Rows[i][13].ToString(); // Column N = index 13
+                var enc = excelData.Rows[i][14].ToString(); // Column O = index 14
+                var saveData = excelData.Rows[i][15].ToString(); // Column O = index 15
+
+                lblCurrentRow.Text = $"Processing Row {i + 1} ..."; // +2 to account for header row
+
+                if (string.IsNullOrWhiteSpace(url) || !string.IsNullOrWhiteSpace(enc) || !string.IsNullOrWhiteSpace(saveData))
+                    continue;
+
+                var row = excelData.Rows[i];
+                if (!string.IsNullOrWhiteSpace(enc) && !enc.StartsWith("ERROR"))
+                {
+                    string saveResult = CallSaveBookingApiAsync(row).Result;
+                    row["Save data"] = "save";
+                    dataGridView1.Refresh();
+                }
+                else
+                {
+                    row["Save dsta"] = "";
+                }
+            }
+
+            //string newColumnName = "Enc Value";
+            //if (excelData.Columns.Contains(newColumnName))
+            //    newColumnName += "_" + Guid.NewGuid().ToString("N").Substring(0, 4);
+
+            //excelData.Columns.Add(newColumnName, typeof(string));
+
+            //foreach (DataRow row in excelData.Rows)
+            //{
+            //    row[newColumnName] = "Default";
+            //}
+
+            //dataGridView1.DataSource = null; // Reset to apply
+            //dataGridView1.DataSource = excelData;
         }
     }
 }
